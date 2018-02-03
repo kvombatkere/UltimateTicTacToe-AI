5e0beace636b44f0427660a96def114a924dfeaa
Avi Webberman <aviwebberman@Avis-MacBook-Pro.local>
2018-02-03 10:13:14 -0500

    quitting option

---
+++ src/MainGame.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+public class MainGame {
+	
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Scanner input = new Scanner(System.in);
+		char gameVersion = 'x';
+		
+		while(true) {
+
+		
+		while(gameVersion != '1' && gameVersion != '2' && gameVersion != '3' && gameVersion != 'q') {
+			
+			System.err.println("What type of Tic-Tac-Toe would you like to play? (Enter 'q' to quit)");
+			System.err.println("(1) Traditional");
+			System.err.println("(2) Advanced");
+			System.err.println("(3) Ultimate");
+			
+			gameVersion = input.next().charAt(0);
+
+			if(gameVersion != '1' && gameVersion != '2' && gameVersion != '3' && gameVersion != 'q') {
+				System.err.println("Please enter a valid option");
+			}
+		
+		}
+		
+
+		if (gameVersion == '1') {
+			TTTGame.main(null);
+		}
+		
+		else if (gameVersion == '2') {
+			//Call main method of version 2 
+			
+		}
+		
+		else if (gameVersion == '3') {
+			//Call main method of version 3
+		}
+		
+		else if (gameVersion == 'q') {
+			System.err.println("Thanks for playing!..Quitting...");
+			break;
+		}
+
+		
+		
+	}
+	}
+	
+}
