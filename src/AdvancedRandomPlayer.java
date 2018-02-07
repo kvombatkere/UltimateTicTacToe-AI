4dc3f088a2c0f4ce1a49d084f7abab1ed9e78635
kvombatkere <kvombatkere@gmail.com>
2018-02-07 00:45:50 -0500

    Created random move player

---
+++ src/AdvancedRandomPlayer.java
@@ -0,0 +1,26 @@
+//Karan Vombatkere, Rebecca Ho Van Dyke + Avram Webberman
+//February 2018
+//CSC 442: AI Project 01 - Tic Tac Toe
+//Basic TTT Game
+
+//Imports
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+
+public class AdvancedRandomPlayer {
+	
+	public AdvancedTTTBoard gameBoard;
+	
+	//Constructor
+	public AdvancedRandomPlayer(AdvancedTTTBoard currentGame) {
+		this.gameBoard = currentGame;
+	}
+	
+	//Method to generate random move
+	public int[] randomMove() {
+		
+		return null;
+	}
+
+}
